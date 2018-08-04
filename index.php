efe4987c4f7d91e00a47640cb57e9f4c0ca7ce37
mzamanzadeh <mohammad.zamanzadeh@gmail.com>
2018-08-04 20:18:23 +0430

    version 1

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: افزونه پرداخت زیبال برای ووکامرس
+Version: 4.5
+Description:  افزونه درگاه پرداخت زیبال برای فروشگاه ساز ووکامرس
+Plugin URI: http://zibal.ir
+Author: Mohammad Zamanzadeh
+Author URI: http://zibal.ir/
+
+*/
+include_once("class-wc-gateway-zibal.php");
